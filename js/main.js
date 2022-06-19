0b18b51fb2ba44894caa6f2ad348ff88887bbf1e
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-06-20 03:48:59 +0500

    mobile finish

--- js/main.js
+++ js/main.js
@@ -3,3 +3,33 @@ if(document.querySelector(".accordion__btn")) {
         $(this).toggleClass('active');
     });
 };
+$(".header__burgir button").click(function() {
+    $(this).toggleClass('active');
+    $(".header_mobile__menu").toggleClass('active');
+    $("body").toggleClass('hidden');
+});
+$(".header_mobile__menu .item").click(function() {
+    $(this).toggleClass('active');
+});
+
+$(".btn_search .open").click(function() {
+    $(".btn_search").addClass('active');
+});
+$(".main").click(function() {
+    $(".header .btn_search").removeClass('active');
+});
+$(".footer").click(function() {
+    $(".header .btn_search").removeClass('active');
+});
+$(".header_mobile__menu ").click(function() {
+    $(".header .btn_search").removeClass('active');
+});
+
+$(".header__navbar .ul_top .item").click(function() {
+    $(".header_desctop__menu").addClass('active');
+    $("body").addClass('hidden');
+});
+$(".header_desctop__menu .close").click(function() {
+    $(".header_desctop__menu").removeClass('active');
+    $("body").removeClass('hidden');
+});
